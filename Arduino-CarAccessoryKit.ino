c221bbaeb6ecb82aae7c23fef82289a59c6588b0
William Taylor <williammarktaylor@gmail.com>
2024-08-07 17:03:56 +1200

    Additional build stuff

--- Arduino-CarAccessoryKit.ino
+++ Arduino-CarAccessoryKit.ino
@@ -5,10 +5,13 @@ William Taylor - 28/07/2024
 
 Manages state of latching relay based on conditions such as:
   * Proximity to a specified Wi-Fi network.
-  * State of GPOI pin.
+  * State of GPIO pin.
   * Run mode selected by way of Bluetooth command.
 Designed with battery as intended power source so focus on energy conservation.
 */
+
+#include <WiFi.h>
+
 void setup() {
 
 }
